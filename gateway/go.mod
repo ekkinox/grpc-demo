ead11d7ae8f7b6339f0ab0b200cd908a41b8772a
amine.abri <amine.abri@ankorstore.com>
2022-05-06 20:59:04 +0100

    - Generating swagger api documentation  for grpc gateway - opening a new port to serve swagger doc config - using swagger ui to read the config file and display the documentation on a html page

--- gateway/go.mod
+++ gateway/go.mod
@@ -3,11 +3,13 @@ module github.com/ekkinox/grpc-demo/gateway
 go 1.18
 
 require (
-	github.com/ekkinox/grpc-demo/proto/go v0.0.0-20220503190901-fc64814b896d
+	github.com/golang/glog v1.0.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
 	github.com/rs/zerolog v1.26.1
 	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21
 	google.golang.org/grpc v1.46.0
+	google.golang.org/protobuf v1.28.0
 )
 
 require (
@@ -15,6 +17,4 @@ require (
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
--- gateway/main.go
+++ gateway/main.go
@@ -2,39 +2,84 @@ package main
 
 import (
 	"context"
-	"net/http"
-	"os"
-
+	_ "embed"
 	pb "github.com/ekkinox/grpc-demo/gateway/proto/go"
+	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/zerolog"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"net/http"
+	"os"
+	"strings"
 )
 
-func main() {
+//go:embed doc/text_tools.swagger.json
+var content []byte
+
+// newGateway returns a new gateway server which translates HTTP into gRPC.
+func newGateway(ctx context.Context, logger zerolog.Logger) (http.Handler, error) {
+	mux := runtime.NewServeMux()
+	dialOpts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	endpoint := "server:50051"
+	// Register TextTool Service
+	logger.Info().Msgf("Registering TextTool Service %s", endpoint)
+	err := pb.RegisterTextToolsServiceHandlerFromEndpoint(ctx, mux, endpoint, dialOpts)
+	if err != nil {
+		logger.Fatal().Err(err)
+	}
+	return mux, nil
+}
+
+// allowCORS allows Cross Origin Resource Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	glog.Infof("preflight request for %s", r.URL.Path)
+	return
+}
+
+// Run starts a HTTP server and blocks forever if successful.
+func Run(address string) error {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Str("component", "text-tool-gateway").Logger()
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	endpoint := "server:50051"
-	port := ":8888"
-
-	// Register TextTool Service
-	logger.Info().Msgf("Registering TextTool Service %s", endpoint)
-	err := pb.RegisterTextToolsServiceHandlerFromEndpoint(ctx, mux, endpoint, opts)
+	mux := http.NewServeMux()
+	// Gateway server
+	gw, err := newGateway(ctx, logger)
 	if err != nil {
-		logger.Fatal().Err(err)
+		return err
 	}
+	mux.Handle("/", gw)
+	// swagger doc
+	mux.HandleFunc("/text_tools.swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write(content)
+	})
 
-	logger.Info().Msgf("TextTool endpoint created, listening on %s", port)
-	handler := http.Handler(mux)
-	err = http.ListenAndServe(port, handler)
-	if err != nil {
+	logger.Info().Msgf("TextTool endpoint created, listening on %s", address)
+	return http.ListenAndServe(address, allowCORS(mux))
+}
+func main() {
+	if err := Run(":8888"); err != nil {
 		logrus.Fatal(err)
 	}
 }
